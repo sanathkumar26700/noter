9f838f6f98ce76622188ecf2f836b16d01fefb5d
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-23 01:50:20 +0530

    Create pages

---
+++ src/Pages/Notes/notes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notes() {
+  return (
+    <h1>Notes</h1>
+  )
+}
+
+export default Notes
---
+++ src/Pages/Trash/trash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Trash() {
+  return (
+    <h1>Trash</h1>
+  )
+}
+
+export default Trash
