271be262ae45899b4e76495a1279aa0fbc8006e0
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-23 01:43:45 +0530

    Create authorized routes

---
+++ src/Routes/navbar-routes.jsx
@@ -0,0 +1,28 @@
+import {Routes, Route} from "react-router-dom"; 
+import Home from "../Pages/Home/home";
+import Notes from "../Pages/Notes/notes"
+import Trash from "../Pages/Trash/trash";
+import Archives from "../Pages/Archives/archives";
+import Login from "../Pages/Authentication/login-page";
+import SignUp from "../Pages/Authentication/sign-up";
+import AuthorizedRoutes from "../Routes/authorized-routes"
+
+function NavBarRoutes() {
+    return (
+    <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/home" element={<Home/>}/>
+        <Route path = "/login-page" element={<Login/>}/>
+        <Route path = "/signup-page" element={<SignUp/>}/>
+
+        <Route element = {<AuthorizedRoutes/>}>
+            <Route path = "/notes" element={<Notes/>}/>
+            <Route path = "/trash" element={<Trash/>}/>
+            <Route path = "/archives" element={<Archives/>}/>
+        </Route>
+
+    </Routes>
+    );
+}
+
+export default NavBarRoutes;
