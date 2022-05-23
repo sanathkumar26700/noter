dcd501da5305f7ecb6df863aa34a9f6d63597a88
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-23 17:31:36 +0530

    Fix : change file name

--- src/Routes/navbar-routes.jsx
+++ src/Routes/navbar-routes.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from "react-router-dom"; 
-import Home from "../Pages/Home/home";
+import Home from '../Pages/Home/Home'
 import Notes from "../Pages/Notes/notes"
 import Trash from "../Pages/Trash/trash";
 import Archives from "../Pages/Archives/archives";
