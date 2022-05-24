4bb73e14d1aea71e046c255498375ffe1297cd82
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-05-24 23:39:34 +0530

    Create tags database

---
+++ src/backend/db/tags.js
@@ -0,0 +1,41 @@
+import { v4 as uuid } from "uuid";
+
+/**
+ * Category Database can be added here.
+ * You can add category of your wish with different attributes
+ * */
+
+export const tags = [
+  {
+    _id: uuid(),
+    tag: "Work"
+  },
+  {
+    _id: uuid(),
+    tag: "Home"
+  },
+  {
+    _id: uuid(),
+    tag: "Ideas"
+  },
+  {
+    _id: uuid(),
+    tag: "Teams"
+  },
+  {
+    _id: uuid(),
+    tag: "School"
+  },
+  {
+    _id: uuid(),
+    tag: "College"
+  },
+  {
+    _id: uuid(),
+    tag: "Health & Wellness"
+  },
+  {
+    _id: uuid(),
+    tag: "Exercise"
+  }
+];
