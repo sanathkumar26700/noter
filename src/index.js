6f6d49631cdd6a9c138f32d9a9bb2cf28f4be088
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-06-02 20:58:55 +0530

    Add userData context

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/authContext";
 import { DataContextProvider } from "./Context/dataContext";
+import { UserDataProvider } from './Context/userDataContext'
 
 // Call make Server
 makeServer();
@@ -15,7 +16,9 @@ ReactDOM.render(
     <BrowserRouter>
       <AuthProvider>
         <DataContextProvider>
-          <App />
+          <UserDataProvider>
+            <App />
+          </UserDataProvider>
         </DataContextProvider>
       </AuthProvider>
     </BrowserRouter>
