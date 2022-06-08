adae0a3efe23cc1ea681106af7876bc6db1c75f7
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2022-06-08 10:56:01 +0530

    Add Trash page

--- src/Pages/Trash/trash.jsx
+++ src/Pages/Trash/trash.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useUserData } from '../../Context/userDataContext'
+import SingleNote from "../../Components/Single note/single-note"
 
-function Trash() {
-  return (
-    <h1>Trash</h1>
+import '../Archives/archives.css'
+
+function Archives() {
+
+  const { userData : {trashData}  } = useUserData()
+  const trashLength = trashData.length
+
+  return(
+    <section className="archive-list__container">
+      {trashLength ?
+      <>
+        <h1 className='archives__section--heading'>Trash ({trashLength})</h1>
+        <section className="archive-list__section">
+          {trashData.map((note) =><SingleNote note = {note} key={note._id}/>)}
+        </section>
+      </>
+          : 
+      <h1 className = "note-list__empty--text">You have no Trash 😉</h1>
+      }
+    </section>
   )
 }
 
-export default Trash
+export default Archives
